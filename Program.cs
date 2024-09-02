3fc8d0183dafbf5bfd764e3c47a3016341e4623a
enzo-giovanne01 <giovanneenzo01@gmail.com>
2024-09-02 00:13:49 -0300

    FOR

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+int n1,n2;
+
+Write("Digite um número: ");
+n1 = Convert.ToInt32(ReadLine());
+Write("Digite um segundo número: ");
+n2 = Convert.ToInt32(ReadLine());
+Clear();
+    if(n1 < n2){
+    for(int i = n1; i<=n2; i++){
+        WriteLine(i.ToString());
+    }
+}
+else{
+    for(int i = n2; i<=n1; i++){
+        WriteLine(i.ToString());
+    }   
+}
+WriteLine("Terminou o laço");
